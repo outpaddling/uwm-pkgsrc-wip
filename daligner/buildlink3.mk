52dfc84d5b0107d37862e2ec9a41782beecf87be
outpaddling <bacon4000@gmail.com>
2017-03-30 11:57:33 -0500

    Clean up

--- daligner/buildlink3.mk
+++ daligner/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	daligner
 .if !defined(DALIGNER_BUILDLINK3_MK)
 DALIGNER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.daligner+=	daligner>=2015.08.17
+BUILDLINK_API_DEPENDS.daligner+=	daligner-2015.10.28{,nb*}
 BUILDLINK_PKGSRCDIR.daligner?=	../../uwm-pkgsrc-wip/daligner
 .endif	# DALIGNER_BUILDLINK3_MK
 
